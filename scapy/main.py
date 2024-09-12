fc428a4c65beffb7094b41494928dde60ccd8d48
M00nWol <ansusthf@gmail.com>
2024-09-12 15:45:12 +0900

    scapy)프로토콜별 실시간 통계 기능 추가

--- scapy/main.py
+++ scapy/main.py
@@ -3,13 +3,34 @@ from scapy.all import sniff, wrpcap
 # list to save packets
 packets = []
 
-# append packet to list
+# count number of packet per protocol
+tcp_count = 0
+udp_count = 0
+icmp_count = 0
+
+# handle packets
 def packet_callback(packet):
+    global tcp_count, udp_count, icmp_count
+
+    # save packets to list
     packets.append(packet)
     print(packet.summary)
 
+    # increase count per protocol
+    if packet.haslayer("TCP"):
+        tcp_count += 1
+    elif packet.haslayer("UDP"):
+        udp_count += 1
+    elif packet.haslayer("ICMP"):
+        icmp_count += 1
+
+    # real-time statistics output
+    print(f"TCP: {tcp_count}. UDP: {udp_count}, ICMP: {icmp_count}")
+    print("-" * 50)
+
+
 # capture packet
-sniff(filter="tcp",prn=packet_callback, count=10)
+sniff(prn=packet_callback, count=50)
 
 # save packets as pcap file
 wrpcap("./captured_packets.pcap", packets)
