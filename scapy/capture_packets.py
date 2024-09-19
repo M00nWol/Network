9473d086922951dde9e3339c2f72f89596301fbc
M00nWol <ansusthf@gmail.com>
2024-09-19 15:26:13 +0900

    scapy) 패킷 src,dst의 IP와 port, 패킷 길이 추출

--- scapy/capture_packets.py
+++ scapy/capture_packets.py
@@ -1,6 +1,8 @@
 # capture_packets.py
-
 from scapy.all import sniff, wrpcap
+from scapy.layers.inet import *
+
+
 
 # list to save packets
 packets = []
@@ -16,20 +18,37 @@ def packet_callback(packet):
 
     # save packets to list
     packets.append(packet)
-    print(packet.summary)
 
-    # increase count per protocol
-    if packet.haslayer("TCP"):
-        tcp_count += 1
-    elif packet.haslayer("UDP"):
-        udp_count += 1
-    elif packet.haslayer("ICMP"):
-        icmp_count += 1
+    if packet.haslayer(IP):
+        # IP 정보 얻어오기
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        packet_length = len(packet)
 
-    # real-time statistics output
-    print(f"TCP: {tcp_count}. UDP: {udp_count}, ICMP: {icmp_count}")
-    print("-" * 50)
+        # 포트 번호 초기화
+        src_port = None
+        dst_port = None
 
+        # 패킷이 TCP/UDP 층인지 체크하고 포트 정보 뽑아내기
+        if packet.haslayer(TCP):
+            src_port = packet[TCP].sport
+            dst_port = packet[TCP].dport
+            tcp_count += 1
+        elif packet.haslayer(UDP):
+            src_port = packet[UDP].sport
+            dst_port = packet[UDP].dport
+            udp_count += 1
+        elif packet.haslayer(ICMP):
+            icmp_count += 1
+
+        # 패킷 정보 뽑아내기
+        print(f"Source IP: {src_ip}, Destination IP: {dst_ip}")
+        if src_port and dst_port:
+            print(f"Source Port: {src_port}, Destination Port: {dst_port}")
+        print(f"Packet Length: {packet_length} bytes")
+        print(packet.summary())
+        print(f"TCP: {tcp_count}, UDP: {udp_count}, ICMP: {icmp_count}")
+        print("-"*50)
 
 # capture packet
 # GUI 사용 안 할 때를 대비해 count와 filter 설정한 것
@@ -39,3 +58,6 @@ def start_capture(count=10, filter="tcp"):
 
     # save packets as pcap file
     wrpcap("./captured_packets.pcap", packets)
+
+if __name__ == "__main__":
+    start_capture(count=10, filter="tcp or udp or icmp")
